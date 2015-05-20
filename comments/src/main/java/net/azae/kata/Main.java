dc5a662b8c4a70e32d09381b1539ecf60b6d3113
Thomas Clavier <tom@tcweb.org>
2015-05-20 21:55:06 +0000

    refacto Main

--- comments/src/main/java/net/azae/kata/Main.java
+++ comments/src/main/java/net/azae/kata/Main.java
@@ -5,13 +5,18 @@ import java.text.ParseException;
 class Main {
 
     public static void main(String[] args) throws ParseException {
-        check(0x80f4, Original.encryptCrc(345, "12/05"));
-        check(0x80f4, Solution.encryptCrc(345, "12/05"));
+    	check(345, "12/05");
+    	check(278, "01/09");
     }
 
-    static void check(int expected, int got) {
-        assert expected == got: "Expected " + Integer.toHexString(expected) + ", got " + Integer.toHexString(got);
+    static void check(int value, String date) throws ParseException {
+    	int expected = Original.encryptCrc(value, date);
+    	int got = Solution.encryptCrc(value, date);
+        if (expected != got) {
+        	System.out.println("Failed. Red. Expected " + Integer.toHexString(expected) + ", got " + Integer.toHexString(got));
+        } else {
+        	System.out.println("OK. Green.");
+        }
     }
-
 }
 

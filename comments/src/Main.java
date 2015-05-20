b28c2e3464f891b441ea502044d2853f1721136e
Olivier Albiez <olivier.albiez@poloper.org>
2015-05-20 16:57:12 +0200

    kata comments added

---
+++ comments/src/Main.java
@@ -0,0 +1,13 @@
+class Main {
+
+    public static void main(String[] args) {
+        check(0x80f4, Original.encryptCrc(345, "12/05"));
+        check(0x80f4, Solution.encryptCrc(345, "12/05"));
+    }
+
+    static void check(int expected, int got) {
+        assert expected == got: "Expected " + Integer.toHexString(expected) + ", got " + Integer.toHexString(got);
+    }
+
+}
+
---
+++ comments/src/Solution.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public static int encryptCrc(int crc, String date) {
+        int key = getMagicNumber(date);
+        return join(encrypt(msb(crc), key), encrypt(lsb(crc), key));
+    }
+
+    private static int getMagicNumber(String date) {
+        return ((Integer.valueOf(date.substring(0, 2)) + Integer.valueOf(date.substring(3, 5))) % 8);
+    }
+
+    private static int lsb(int value) {
+        return value & 0xFF;
+    }
+
+    private static int msb(int value) {
+        return (value >> 8) & 0xFF;
+    }
+
+    private static int join(int msb, int lsb) {
+        return msb << 8 | lsb;
+    }
+
+    private static int encrypt(int value, int count) {
+        value = rotateRight(value, count);
+        return (value ^ (value << 1)) & 0xFF;
+    }
+
+    private static int rotateRight(int value, int count) {
+        for (int i = 0; i < count; ++i) {
+            if ((value & 0x01) != 0)
+                value = (0x80 | (value >> 1));
+            else
+                value = (value >> 1);
+        }
+        return value;
+    }
+
+}
+

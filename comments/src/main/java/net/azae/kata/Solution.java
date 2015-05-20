d8d8454b05f1d360b3e72f00d29ff9d5cefa71d1
Thomas Clavier <tom@tcweb.org>
2015-05-20 23:10:34 +0200

    static import and Calendar not private

--- comments/src/main/java/net/azae/kata/Solution.java
+++ comments/src/main/java/net/azae/kata/Solution.java
@@ -4,19 +4,21 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import static java.util.Calendar.DAY_OF_MONTH;
+import static java.util.Calendar.MONTH;
 
 class Solution {
     private final static SimpleDateFormat formatter = new SimpleDateFormat("dd/MM");
-    private final static Calendar calendar = Calendar.getInstance();
-
+    
     public static int encryptCrc(int crc, String date) throws ParseException {
         int key = getMagicNumber(formatter.parse(date));
         return join(encrypt(mostSignificantBit(crc), key), encrypt(leastSignificantBit(crc), key));
     }
 
     private static int getMagicNumber(Date date) {
+    	Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
-        return ((calendar.get(Calendar.DAY_OF_MONTH) + calendar.get(Calendar.MONTH)) % 8);
+        return ((calendar.get(DAY_OF_MONTH) + calendar.get(MONTH)) % 8);
     }
 
     private static int leastSignificantBit(int value) {

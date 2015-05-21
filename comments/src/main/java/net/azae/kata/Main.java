b5f5da31d75e5745072a7b55277abddbc7253988
Olivier Albiez <olivier.albiez@poloper.org>
2015-05-21 11:54:28 +0200

    comments: tests cases added

--- comments/src/main/java/net/azae/kata/Main.java
+++ comments/src/main/java/net/azae/kata/Main.java
@@ -5,18 +5,25 @@ import java.text.ParseException;
 class Main {
 
     public static void main(String[] args) throws ParseException {
-        check(345, "12/05");
-    	check(278, "01/09");
+        check(0x0000, "12/05");
+        check(0x0001, "01/08");
+        check(0x3408, "12/05");
+    	check(0xFFFF, "01/07");
     }
 
     static void check(int value, String date) throws ParseException {
     	int expected = Original.encryptCrc(value, date);
     	int got = Solution.encryptCrc(value, date);
         if (expected != got) {
-        	System.out.println("Failed. Red. Expected " + Integer.toHexString(expected) + ", got " + Integer.toHexString(got));
+        	System.out.println("Failed. Red: Expected " + hex(expected) + ", got " + hex(got));
         } else {
-        	System.out.println("OK. Green.");
+        	System.out.println("OK. Green: " + hex(got));
         }
     }
+
+    public static String hex(int value) {
+        return String.format("0x%4s", Integer.toHexString(value)).replace(" ", "0");
+    }
+
 }
 

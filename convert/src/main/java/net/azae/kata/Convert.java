9d04a8435f380ed1b693fce0d2759f7435848f07
Thomas Clavier <tom@tcweb.org>
2015-05-26 21:08:47 +0200

    reinit project

--- convert/src/main/java/net/azae/kata/Convert.java
+++ convert/src/main/java/net/azae/kata/Convert.java
@@ -1,18 +1,16 @@
 package net.azae.kata;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Convert {
-	
 	public static final char[] NUM_0_9 = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 	public static String[] TEXT_0_9 = new String[] { "zéro", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf" };
 	public static String[] TEXT_10_19 = new String[] { "dix", "onze", "douze", "treize", "quatorze", "quinze", "seize", "dix sept", "dix huit",	"dix neuf" };
 	public static String[] TEXT_10_90 = new String[] { "", "dix", "vingt", "trente", "quarante", "cinquante", "soixante", "soixante", "quatre vingt", "quatre vingt" };
 	public static String[] TEXT_10_99 = new String[] { "", " et un", " deux", " trois", " quatre", " cinq", " six", " sept", " huit", " neuf" };
-	public static String[] MAGNITUDE = new String[] { "", "mille", "million", "milliard", "billion", "billiar", "trillion", "trilliard", "quadrillion", "quadrilliard" };
+	public static String[] TEXT_1000 = new String[] { "", "mille", "million", "milliard", "billion", "billiar", "trillion", "trilliard", "quadrillion", "quadrilliard" };
 	private static Map<Character, String> num2text09;
 	private static Map<Character, String> num2text1019;
 	private static Map<Character, String> num2text1090;
@@ -20,82 +18,50 @@ public class Convert {
 
 	/**
 	 * Returns a text version of a number passed as input.
-	 * 
+	 *
 	 * @param input The number to convert to text.
 	 * @return Text version of the number.
 	 */
 	public static String num2text(String input) {
 		if (num2text09 == null)
 			loadNum2text();
-		if (!isNumeric(input)) return null;
+		if (!estNum(input))
+			return null;
+		input.split("(?<=\\G...)");
+		StringBuilder sb = new StringBuilder(input).reverse();
 		String retour = "";
-		List<String> blocks = split(input);
-		for (int blockNumber = 0; blockNumber < blocks.size(); blockNumber++) {
-			retour = processBlock( blocks.get(blockNumber), blockNumber, blocks.size()) + retour;
+		for (int i = 0; i < (sb.length() - 1) / 3 + 1; i++) {
+			retour = (trois2text(new StringBuilder(sb.substring(i * 3, ((i * 3 + 3 > sb.length()) ? sb.length() : i * 3 + 3))).reverse().toString()).equals("zéro") && (sb.length() - 1) / 3 + 1 > 1) ? retour : (trois2text(new StringBuilder(sb.substring(i * 3, (i * 3 + 3 > sb.length()) ? sb.length() : i * 3 + 3)).reverse().toString()).equals("un") && i != 0) ? TEXT_1000[i] + " "	+ retour : trois2text(new StringBuilder(sb.substring(i * 3,	(i * 3 + 3 > sb.length()) ? sb.length()	: i * 3 + 3)).reverse().toString())	+ ((i == 0) ? "" : " ")	+ TEXT_1000[i] + " " + retour;
 		}
-		return retour.trim();
+		return retour.substring(0, retour.length() - 1);
 	}
 
-	private static String processBlock( String block, int i, int total) {
-		 if (renderBlock(block).equals("zéro") && total > 1) 
-			 return "";
-		 else 
-			 return concatMagnitudeName(renderBlock(block), i);
+	private static boolean estNum(String input) {
+		Set<Character> keySet = num2text09.keySet();
+		for (int i = 0; i < input.length(); i++)
+			if (!keySet.contains(input.charAt(i)))
+				return false;
+		return true;
 	}
 
-	private static String concatMagnitudeName(String blockText, int i) {
-		String prefix = blockText + " ";
-		if (blockText.equals("un") && i > 0 ) {
-			prefix = "";
-		}
-		String suffix = "";
-		if (i > 0) 
-			suffix = " ";
-		return prefix + MAGNITUDE[i] + suffix;
-	
-	}
-	
-	private static List<String> split(String input) {
-		StringBuilder revertedInput = new StringBuilder(input).reverse();
-		List<String> output = new ArrayList<>();
-		for (int i = 0; i < (revertedInput.length() - 1) / 3 + 1; i++) {
-			int end = Math.min(i*3 +3, revertedInput.length());
-			String orderedBloc = new StringBuilder(revertedInput.substring(i*3, end)).reverse().toString();
-			output.add(orderedBloc);
-		}
-		return output;
-	}
-	
-	private static boolean isNumeric(String input) {
-		return input.matches("^[0-9]+$");
-	}
-
-	private static String renderBlock(String input) {
+	private static String trois2text(String input) {
 		if (input.length() == 1)
 			return un2text(input);
 		if (input.length() == 2) {
-			return renderDecade(input);
+			return deux2text(input);
 		}
 		if (input.length() == 3) {
-			String decade = renderDecade(input.substring(1, 3));
-			String prefix = "";
 			if (input.charAt(0) == '0')
-				return decade;
-			else if (input.charAt(0) == '1') {
-				prefix = "cent";
-			} else {
-				prefix = num2text09.get(input.charAt(0)) + " cent";
-			}
-			if (decade.equals("zéro")) {
-				return prefix;
-			} else {
-				return prefix + " " + decade;
-			}
+				return trois2text(input.substring(1, 3));
+			else if (input.charAt(0) == '1')
+				return "cent" + ((trois2text(input.substring(1, 3)).equals("zéro")) ? "" : " " + trois2text(input.substring(1, 3)));
+			else
+				return num2text09.get(input.charAt(0)) + " cent" + ((trois2text(input.substring(1, 3)).equals("zéro")) ? ""	: " " + trois2text(input.substring(1, 3)));
 		}
 		return null;
 	}
 
-	private static String renderDecade(String input) {
+	private static String deux2text(String input) {
 		if (input.length() == 2) {
 			if (input.charAt(0) == '7' || input.charAt(0) == '9')
 				return num2text1090.get(input.charAt(0)) + " " + num2text1019.get(input.charAt(1));
--- convert/src/test/java/net/azae/kata/TestsBuilder.java
+++ convert/src/test/java/net/azae/kata/TestsBuilder.java
@@ -1,21 +1,5 @@
 package net.azae.kata;
 
 public class TestsBuilder {
-	public static void main(String[] args) {
-		for (int i = 1; i <= 1003; i++) {
-			printTest(Convert.num2text(Integer.toString(i)), Integer.toString(i));
-		}
-		
-		printTest(Convert.num2text("10078"), "10078");
-		printTest(Convert.num2text("2007008"), "2007008");
-		printTest(Convert.num2text("1007008"), "1007008");
-		printTest(Convert.num2text("4000700008"), "4000700008");
-		printTest(Convert.num2text("9000000700008"), "9000000700008");
-		printTest(Convert.num2text("9012300000700008"), "9012300000700008");
-		
-	}
 
-	private static void printTest(String expected, String input) {
-		System.out.println("assertEquals(\"" + expected	+ "\", ConvertRefactored.num2text(\"" + input + "\"));");
-	}
 }

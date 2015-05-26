36f734bdb1370eb2c631558897e77aefb1b7cffe
Thomas Clavier <tom@tcweb.org>
2015-05-26 23:35:12 +0200

    refacto fini

--- convert/src/test/java/net/azae/kata/TestsBuilder.java
+++ convert/src/test/java/net/azae/kata/TestsBuilder.java
@@ -2,7 +2,21 @@ package net.azae.kata;
 
 public class TestsBuilder {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2000; i++) {
+            printAssert(Integer.toString(i));
+        }
+        printAssert("10009");
+        printAssert("12300012130006780009");
+        printAssert("az0009");
 
-	}
+    }
+
+    private static void printAssert(String input) {
+        String expected = Convert.num2text(input);
+        if (expected != null)
+            System.out.println("assertEquals(\"" + expected + "\", Convert.num2text(\"" + input + "\"));");
+        else
+            System.out.println("assertEquals(null, Convert.num2text(\"" + input + "\"));");
+    }
 }

e13cf275192443ab0297840157d5f26c0d0fda49
Olivier Albiez <olivier.albiez@poloper.org>
2015-08-10 15:23:44 +0200

    kata FooBarQix added

---
+++ foobarqix/src/main/java/net/azae/kata/foobarqix/FooBarQix.java
@@ -0,0 +1,77 @@
+package net.azae.kata.foobarqix;
+
+import java.util.function.Predicate;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Integer.valueOf;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Stream.concat;
+import static java.util.stream.Stream.of;
+
+public final class FooBarQix {
+
+    public static final String[] VALUES = new String[]{"*", "", "", "Foo", "", "Bar", "", "Qix", "", ""};
+
+    private FooBarQix() {
+    }
+
+    public static String compute(final String value) {
+        return computeWithStream(value);
+    }
+
+
+    public static String computeWithStream(final String value) {
+        final int number = valueOf(value);
+        final Predicate<Integer> isNumberDivisibleBy = n -> number % n == 0;
+        final String result = concat(
+                of(3, 5, 7).filter(isNumberDivisibleBy),
+                stream(value.split("")).map(Integer::valueOf)
+        ).map(v -> VALUES[v]).collect(joining());
+
+        if (result.matches("\\**")) {
+            return value.replace('0', '*');
+        } else {
+            return result;
+        }
+    }
+
+    public static String computeV1(final String value) {
+        final int number = parseInt(value);
+        final StringBuilder builder = new StringBuilder();
+        addWhenDivisible(number, 3, "Foo", builder);
+        addWhenDivisible(number, 5, "Bar", builder);
+        addWhenDivisible(number, 7, "Qix", builder);
+        boolean substitute = builder.length() == 0;
+        for (final char c : value.toCharArray()) {
+            switch (c) {
+                case '3':
+                    builder.append("Foo");
+                    substitute = false;
+                    break;
+                case '5':
+                    builder.append("Bar");
+                    substitute = false;
+                    break;
+                case '7':
+                    builder.append("Qix");
+                    substitute = false;
+                    break;
+                case '0':
+                    builder.append('*');
+                    break;
+            }
+        }
+        if (substitute) {
+            return value.replace('0', '*');
+        } else {
+            return builder.toString();
+        }
+    }
+
+    private static void addWhenDivisible(final int value, final int number, final String literal, final StringBuilder builder) {
+        if (value % number == 0) {
+            builder.append(literal);
+        }
+    }
+}

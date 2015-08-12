cdef0371a7726f04b91fb755569227d0e781bf56
Olivier Albiez <olivier.albiez@poloper.org>
2015-08-12 17:04:45 +0200

    RPN Calculator updated

--- rpn/src/main/java/net/azae/kata/rpn/RPNCalculator.java
+++ rpn/src/main/java/net/azae/kata/rpn/RPNCalculator.java
@@ -1,91 +1,26 @@
 package net.azae.kata.rpn;
 
-import java.util.Collections;
+import java.util.NoSuchElementException;
 import java.util.Stack;
 
-import static java.util.Arrays.stream;
-
 public final class RPNCalculator {
-
-    interface Operator {
-        int compute(final String atom, final Stack<Integer> stack);
-    }
-
-    enum Operators implements Operator {
-        LITERAL("\\d+") {
-            public int compute(final String atom, final Stack<Integer> stack) {
-                return Integer.valueOf(atom);
-            }
-        },
-        PLUS("\\+") {
-            public int compute(final String atom, final Stack<Integer> stack) {
-                return stack.pop() + stack.pop();
-            }
-        },
-        MINUS("-") {
-            public int compute(final String atom, final Stack<Integer> stack) {
-                final int a = stack.pop();
-                final int b = stack.pop();
-                return b - a;
-            }
-        },
-        MULTIPLY("\\*") {
-            public int compute(final String atom, final Stack<Integer> stack) {
-                return stack.pop() * stack.pop();
-            }
-        },
-        DIVIDE("/") {
-            public int compute(final String atom, final Stack<Integer> stack) {
-                final int a = stack.pop();
-                final int b = stack.pop();
-                return b / a;
-            }
-        },
-        SQRT("SQRT") {
-            public int compute(final String atom, final Stack<Integer> stack) {
-                return (int) Math.sqrt(stack.pop());
-            }
-        },
-        MAX("MAX") {
-            public int compute(final String atom, final Stack<Integer> stack) {
-                final int result = Collections.max(stack);
-                stack.clear();
-                return result;
-            }
-        },
-        ERROR(".*") {
-            public int compute(final String atom, final Stack<Integer> stack) {
-                throw new RuntimeException("Unknwon operator " + atom);
-            }
-        };
-
-        private final String pattern;
-
-        Operators(final String pattern) {
-            this.pattern = pattern;
-        }
-
-        public boolean matches(final String atom) {
-            return atom.matches(pattern);
-        }
-    }
-
-
-    private RPNCalculator() {
-    }
-
     public static int compute(final String expression) {
         final Stack<Integer> stack = new Stack<>();
-        stream(expression.split(" ")).forEachOrdered(atom -> processAtom(atom, stack));
+        for (final String atom : expression.split(" ")) {
+            stack.add(tokenize(atom).process(atom, stack));
+        }
         return stack.pop();
     }
 
-    private static void processAtom(final String atom, final Stack<Integer> stack) {
-        for (final Operators operator : Operators.values()) {
-            if (operator.matches(atom)) {
-                stack.add(operator.compute(atom, stack));
-                return;
+    private static Token tokenize(final String atom) {
+        for (final Tokens token : Tokens.values()) {
+            if (token.matches(atom)) {
+                return token;
             }
         }
+        throw new NoSuchElementException(atom);
+    }
+
+    private RPNCalculator() {
     }
 }
--- rpn/src/test/java/net/azae/kata/rpn/RPNCalculatorTest.java
+++ rpn/src/test/java/net/azae/kata/rpn/RPNCalculatorTest.java
@@ -2,49 +2,57 @@ package net.azae.kata.rpn;
 
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
+
+import static net.azae.kata.rpn.RPNCalculator.compute;
 import static org.junit.Assert.assertEquals;
 
 public class RPNCalculatorTest {
 
     @Test
     public void testConst() {
-        assertEquals(1, RPNCalculator.compute("1"));
+        assertEquals(1, compute("1"));
     }
 
     @Test
     public void testAdd() {
-        assertEquals(5, RPNCalculator.compute("2 3 +"));
+        assertEquals(5, compute("2 3 +"));
     }
 
     @Test
     public void testSub() {
-        assertEquals(6, RPNCalculator.compute("9 3 -"));
+        assertEquals(6, compute("9 3 -"));
     }
 
     @Test
     public void testMul() {
-        assertEquals(6, RPNCalculator.compute("2 3 *"));
+        assertEquals(6, compute("2 3 *"));
     }
 
     @Test
     public void testDiv() {
-        assertEquals(2, RPNCalculator.compute("6 3 /"));
+        assertEquals(2, compute("6 3 /"));
     }
 
     @Test
     public void testComplexExpression() {
-        assertEquals(3, RPNCalculator.compute("4 2 + 3 -"));
-        assertEquals(141, RPNCalculator.compute("3 5 8 * 7 + *"));
+        assertEquals(3, compute("4 2 + 3 -"));
+        assertEquals(141, compute("3 5 8 * 7 + *"));
     }
 
     @Test
     public void testSqrt() {
-        assertEquals(2, RPNCalculator.compute("4 SQRT"));
+        assertEquals(2, compute("4 SQRT"));
     }
 
     @Test
     public void testMax() {
-        assertEquals(12, RPNCalculator.compute("10 2 3 8 12 MAX"));
+        assertEquals(12, compute("10 2 3 8 12 MAX"));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testError() {
+        compute("a");
     }
 
 }

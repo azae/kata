e7b6e2e08294db0f53e8d3f914f84b081a240c3b
Olivier Albiez <olivier.albiez@poloper.org>
2015-08-17 16:40:12 +0200

    hexagonal: comment removed

--- kata-hexagonal/src/test/java/net/azae/kata/hexagonal/CreditLogicUnitTest.java
+++ kata-hexagonal/src/test/java/net/azae/kata/hexagonal/CreditLogicUnitTest.java
@@ -6,17 +6,16 @@ import org.junit.Test;
 public class CreditLogicUnitTest {
     @Test
     public void testAcceptedCreditWhenGoodBalanceAndGoodSalary() {
-        Assert.assertTrue("credit should be allowed", CreditLogic.allowCredit(100, 10, 1000, 500));
+        Assert.assertTrue(CreditLogic.allowCredit(100, 10, 1000, 500));
     }
 
     @Test
     public void testRejectedCreditWhenBalanceTooLow() {
-        Assert.assertFalse("credit should be rejected", CreditLogic.allowCredit(100, 10, 0, 500));
+        Assert.assertFalse(CreditLogic.allowCredit(100, 10, 0, 500));
     }
 
     @Test
     public void testRejectedCreditWhenSalaryTooLow() {
-        Assert.assertFalse("credit should be rejected", CreditLogic.allowCredit(100, 10, 1000, 10));
+        Assert.assertFalse(CreditLogic.allowCredit(100, 10, 1000, 10));
     }
-
 }

d78b4ca7b377cde1d1610d67e23c31dd7a3722f1
Olivier Albiez <olivier.albiez@poloper.org>
2015-08-18 19:06:00 +0200

    gameoflife: README added and test refactored

--- gameoflife/src/test/java/net/azae/kata/gameoflife/GameOfLifeTest.java
+++ gameoflife/src/test/java/net/azae/kata/gameoflife/GameOfLifeTest.java
@@ -11,90 +11,89 @@ public class GameOfLifeTest {
     @Test
     public void testEmptyBoard() {
         assertEvolve(1,
-                "....", "....",
-                "....", "....",
-                "....", "....",
-                "....", "...."
+                "  ....  ....  ",
+                "  ....  ....  ",
+                "  ....  ....  ",
+                "  ....  ....  "
         );
     }
 
     @Test
     public void testOneCell() {
         assertEvolve(2,
-                "....", "....", "....",
-                ".*..", "....", "....",
-                "....", "....", "....",
-                "....", "....", "...."
+                "  ....  ....  ....  ",
+                "  .*..  ....  ....  ",
+                "  ....  ....  ....  ",
+                "  ....  ....  ....  "
         );
     }
 
     @Test
     public void testOneGeneration() {
         assertEvolve(2,
-                "....", "....", "....",
-                ".**.", ".**.", ".**.",
-                ".*..", ".**.", ".**.",
-                "....", "....", "...."
+                "  ....  ....  ....  ",
+                "  .**.  .**.  .**.  ",
+                "  .*..  .**.  .**.  ",
+                "  ....  ....  ....  "
         );
     }
 
     @Test
     public void testStableScenario() {
         assertEvolve(1,
-                "....", "....",
-                ".**.", ".**.",
-                ".**.", ".**.",
-                "....", "...."
+                "  ....  ....  ",
+                "  .**.  .**.  ",
+                "  .**.  .**.  ",
+                "  ....  ....  "
         );
     }
 
     @Test
     public void testBeehiveScenario() {
         assertEvolve(1,
-                "......", "......",
-                "..**..", ".*...*",
-                ".*..*.", ".*..*.",
-                "..**..", "*...*.",
-                "......", "......");
+                "  ......  ......  ",
+                "  ..**..  .*...*",
+                "  .*..*.  .*..*.  ",
+                "  ..**..  *...*.  ",
+                "  ......  ......  ");
     }
 
     @Test
     public void testLoafScenario() {
         assertEvolve(1,
-                "......", "......",
-                "..**..", "..*...",
-                ".*..*.", ".*.*..",
-                "..*.*.", ".*..*.",
-                "...*..", "..**..",
-                "......", "......");
+                "  ......  ......  ",
+                "  ..**..  ..*...  ",
+                "  .*..*.  .*.*..  ",
+                "  ..*.*.  .*..*.  ",
+                "  ...*..  ..**..  ",
+                "  ......  ......  ");
     }
 
     @Test
     public void testBoatScenario() {
         assertEvolve(1,
-                ".....", ".....",
-                ".**..", ".**..",
-                ".*.*.", ".*.*.",
-                "..*..", "..*..",
-                ".....", ".....");
+                "  .....  .....  ",
+                "  .**..  .**..  ",
+                "  .*.*.  .*.*.  ",
+                "  ..*..  ..*..  ",
+                "  .....  .....  ");
     }
 
     private static void assertEvolve(final int generations, final String... data) {
-        final int step = generations + 1;
-        final int height = data.length / step;
-        final int width = data[0].length();
-        Board current = extractBoard(width, height, data, 0, step);
+        Board current = extractBoard(data, 0);
         for (int i = 1; i <= generations; i++) {
             current = evolve(current);
-            assertEquals(extractBoard(width, height, data, i, step), current);
+            assertEquals(extractBoard(data, i), current);
         }
     }
 
-    private static Board extractBoard(final int width, final int height, final String[] data, final int starting, final int step) {
+    private static Board extractBoard(final String[] data, final int inex) {
+        final int height = data.length;
         final StringBuilder builder = new StringBuilder();
-        for (int i = starting; i < data.length; i += step) {
-            builder.append(data[i]);
+        for (String d : data) {
+            builder.append(d.trim().split("\\s+")[inex].trim());
         }
+        final int width = builder.length() / height;
         return board(width, height, builder.toString());
     }
 }

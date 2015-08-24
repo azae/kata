0a6bf15d1b31a52d828089124117503563d510ce
Thomas Clavier <tom@tcweb.org>
2015-08-24 16:59:14 +0200

    add 2 numbers : refacto

--- stringcalculator/src/main/java/net/azae/kata/stringcalculator/StringCalculator.java
+++ stringcalculator/src/main/java/net/azae/kata/stringcalculator/StringCalculator.java
@@ -1,10 +1,13 @@
 package net.azae.kata.stringcalculator;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class StringCalculator {
     public static double add(String number) {
-        String[] numbers = number.split(",");
+        Double[] numbers = Arrays.stream(number.split(",")).map(Double::parseDouble).toArray(Double[]::new);
         if (numbers.length == 0) return 0;
-        if (numbers.length == 1) return Double.parseDouble(number);
-        return Double.parseDouble(numbers[0]) + Double.parseDouble(numbers[1]);
+        if (numbers.length == 1) return numbers[0];
+        return numbers[0] + numbers[1];
     }
 }

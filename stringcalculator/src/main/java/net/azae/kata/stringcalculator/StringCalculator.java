619130ae71cd5c3882a010c28b64a5233c1fd101
Thomas Clavier <tom@tcweb.org>
2015-08-24 17:06:40 +0200

    add many numbers : refacto

--- stringcalculator/src/main/java/net/azae/kata/stringcalculator/StringCalculator.java
+++ stringcalculator/src/main/java/net/azae/kata/stringcalculator/StringCalculator.java
@@ -5,9 +5,7 @@ import java.util.stream.Collectors;
 
 public class StringCalculator {
     public static double add(String number) {
-        Double[] numbers = Arrays.stream(number.split(",")).map(Double::parseDouble).toArray(Double[]::new);
-        if (numbers.length == 0) return 0;
-        if (numbers.length == 1) return numbers[0];
-        return numbers[0] + numbers[1];
+        if (number.isEmpty()) return 0;
+        return Arrays.stream(number.split(",")).map(Double::parseDouble).reduce(Double::sum).orElse(null);
     }
 }
--- stringcalculator/src/test/java/net/azae/kata/stringcalculator/StringCalculatorTest.java
+++ stringcalculator/src/test/java/net/azae/kata/stringcalculator/StringCalculatorTest.java
@@ -20,6 +20,14 @@ public class StringCalculatorTest {
     @Test
     public void add_should_return_sum_when_input_are_2_numbers_separated_by_comma () {
         assertAdd(4.0, "1,3");
+        assertAdd(4.4, "1.1,3.3");
+
+    }
+
+    @Test
+    public void add_should_return_sum_when_input_are_more_than_2_numbers_separated_by_comma () {
+        assertAdd(4.0, "1,2,1");
+        assertAdd(5.0, "1,2,1,1");
     }
 
     public void assertAdd(double expected, String actual) {

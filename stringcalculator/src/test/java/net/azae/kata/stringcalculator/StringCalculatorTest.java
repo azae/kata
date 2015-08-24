e5e98856bf1b60bdaa3fa82b6a794d71d4dfb4de
Thomas Clavier <tom@tcweb.org>
2015-08-24 16:38:39 +0200

    0 when empty : green

--- stringcalculator/src/test/java/net/azae/kata/stringcalculator/StringCalculatorTest.java
+++ stringcalculator/src/test/java/net/azae/kata/stringcalculator/StringCalculatorTest.java
@@ -6,6 +6,6 @@ import org.testng.annotations.Test;
 public class StringCalculatorTest {
     @Test
     public void add_should_return_0_when_input_is_empty () {
-        Assert.assertEquals(0,StringCalculator.add(""));
+        Assert.assertEquals(0.0,StringCalculator.add(""));
     }
 }

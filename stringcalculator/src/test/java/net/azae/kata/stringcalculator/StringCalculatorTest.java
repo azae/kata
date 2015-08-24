8b7ee7227ec38d0c3e46e495f3bb5171a89a5316
Thomas Clavier <tom@tcweb.org>
2015-08-24 16:40:33 +0200

    0 when empty : refacto

--- stringcalculator/src/test/java/net/azae/kata/stringcalculator/StringCalculatorTest.java
+++ stringcalculator/src/test/java/net/azae/kata/stringcalculator/StringCalculatorTest.java
@@ -3,9 +3,16 @@ package net.azae.kata.stringcalculator;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static net.azae.kata.stringcalculator.StringCalculator.add;
+import static org.testng.Assert.assertEquals;
+
 public class StringCalculatorTest {
     @Test
     public void add_should_return_0_when_input_is_empty () {
-        Assert.assertEquals(0.0,StringCalculator.add(""));
+        assertAdd(0.0, "");
+    }
+
+    public void assertAdd(double expected, String actual) {
+        assertEquals(expected, add(actual));
     }
 }

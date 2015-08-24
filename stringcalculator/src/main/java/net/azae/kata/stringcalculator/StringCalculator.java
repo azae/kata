109e4f121fd1f948a344f4d8fb9fe9c1e3f4a80f
Thomas Clavier <tom@tcweb.org>
2015-08-24 16:38:02 +0200

    0 when empty : red

---
+++ stringcalculator/src/main/java/net/azae/kata/stringcalculator/StringCalculator.java
@@ -0,0 +1,7 @@
+package net.azae.kata.stringcalculator;
+
+public class StringCalculator {
+    public static double add(String number) {
+        return -1;
+    }
+}
---
+++ stringcalculator/src/test/java/net/azae/kata/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,11 @@
+package net.azae.kata.stringcalculator;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class StringCalculatorTest {
+    @Test
+    public void add_should_return_0_when_input_is_empty () {
+        Assert.assertEquals(0,StringCalculator.add(""));
+    }
+}

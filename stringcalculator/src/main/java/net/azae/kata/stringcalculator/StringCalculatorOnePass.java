fffd963b95e47920a2562cd02d0c71327feaf5fb
Olivier Albiez <olivier.albiez@poloper.org>
2015-08-26 15:05:51 +0200

    StringCalculator updated: one pass version corrected

--- stringcalculator/src/main/java/net/azae/kata/stringcalculator/StringCalculatorOnePass.java
+++ stringcalculator/src/main/java/net/azae/kata/stringcalculator/StringCalculatorOnePass.java
@@ -1,17 +1,36 @@
 package net.azae.kata.stringcalculator;
 
 import java.security.InvalidParameterException;
+import java.util.Arrays;
 
 public class StringCalculatorOnePass {
+
     public static double add(final String operands) {
         if (operands.isEmpty()) {
             return 0;
         }
         double sum = 0;
-        for (final String operand : operands.split("(,|\n)")) {
+        for (final String operand : split(operands)) {
             sum += toDouble(operand);
         }
         return sum;
+    }
+
+    public static double addWithStream(final String operands) {
+        if (operands.isEmpty()) {
+            return 0;
+        }
+        return Arrays.stream(split(operands)).mapToDouble(v -> toDouble(v)).sum();
+    }
+
+    private static String[] split(final String operands) {
+        if (operands.startsWith("//")) {
+            if (operands.charAt(3) != '\n') {
+                throw new InvalidParameterException();
+            }
+            return operands.substring(4).split("[" + operands.charAt(2) + "\n]", -1);
+        }
+        return operands.split("[,\n]", -1);
     }
 
     private static double toDouble(final String value) {

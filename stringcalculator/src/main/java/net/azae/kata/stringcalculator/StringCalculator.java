772d11c9a507a904390842a9e3d0eb7b10952eb8
Olivier Albiez <olivier.albiez@poloper.org>
2015-08-26 10:18:00 +0200

    StringCalculator updated: one pass version added

--- stringcalculator/src/main/java/net/azae/kata/stringcalculator/StringCalculator.java
+++ stringcalculator/src/main/java/net/azae/kata/stringcalculator/StringCalculator.java
@@ -4,20 +4,20 @@ import java.security.InvalidParameterException;
 import java.util.Arrays;
 
 public class StringCalculator {
-    public static double add(final String number) {
-        if (number.isEmpty()) return 0;
-        final long numbersOfOperandes = countOperandes(number);
-        final long numbersOfSeparators = countSeparators(number);
-        if (numbersOfOperandes <= numbersOfSeparators)
+    public static double add(final String input) {
+        if (input.isEmpty()) return 0;
+        final long numbersOfOperands = countOperands(input);
+        final long numbersOfSeparators = countSeparators(input);
+        if (numbersOfOperands <= numbersOfSeparators)
             throw new InvalidParameterException();
-        return Arrays.stream(number.split("(,|\n)")).map(Double::parseDouble).reduce(Double::sum).orElse(null);
+        return Arrays.stream(input.split("(,|\n)")).map(Double::parseDouble).reduce(Double::sum).orElse(null);
     }
 
     static long countSeparators(final String number) {
         return number.length() - number.replace(",", "").replace("\n", "").length();
     }
 
-    static long countOperandes(final String number) {
+    static long countOperands(final String number) {
         return Arrays.stream(number.split("(,|\n)")).count();
     }
 
--- stringcalculator/src/main/java/net/azae/kata/stringcalculator/StringCalculatorOnePass.java
+++ stringcalculator/src/main/java/net/azae/kata/stringcalculator/StringCalculatorOnePass.java
@@ -3,12 +3,12 @@ package net.azae.kata.stringcalculator;
 import java.security.InvalidParameterException;
 
 public class StringCalculatorOnePass {
-    public static double add(final String numbers) {
-        if (numbers.isEmpty()) {
+    public static double add(final String operands) {
+        if (operands.isEmpty()) {
             return 0;
         }
         double sum = 0;
-        for (final String operand : numbers.split("(,|\n)")) {
+        for (final String operand : operands.split("(,|\n)")) {
             if (operand.isEmpty()) {
                 throw new InvalidParameterException();
             }

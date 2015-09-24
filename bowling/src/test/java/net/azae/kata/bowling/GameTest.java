60e13f7cea5a67bf6fce02fa801b8cec1eed9d1b
Olivier Albiez <olivier.albiez@poloper.org>
2015-09-24 18:39:17 +0200

    bowling: dsl generalisation

--- bowling/src/test/java/net/azae/kata/bowling/GameTest.java
+++ bowling/src/test/java/net/azae/kata/bowling/GameTest.java
@@ -10,61 +10,51 @@ public class GameTest {
 
     @Test
     public void ensure_score_is_zero_when_no_pin() {
-        assertEquals(scoreOf(rolls(20, 0)), 0);
+        assertEquals(scoreOf(repeat(20, roll(0))), 0);
     }
 
     @Test
     public void ensure_score_is_sum_of_pin_when_no_bonnus() {
-        assertEquals(scoreOf(rolls(20, 1)), 20);
+        assertEquals(scoreOf(repeat(20, roll(1))), 20);
     }
 
     @Test
     public void ensure_score_take_spare_bonus() {
-        assertEquals(scoreOf(spare(5), roll(2), rolls(17, 0)), 14);
+        assertEquals(scoreOf(spare(5), roll(2), repeat(17, roll(0))), 14);
     }
 
     @Test
     public void ensure_score_take_strike_bonus() {
-        assertEquals(scoreOf(strike(), roll(3), roll(2), rolls(18, 0)), 20);
+        assertEquals(scoreOf(strike(), roll(3), roll(2), repeat(18, roll(0))), 20);
     }
 
     @Test
     public void compute_max_score() {
-        assertEquals(scoreOf(strikes(12)), 300);
+        assertEquals(scoreOf(repeat(12, strike())), 300);
     }
 
-    private static Consumer<Game> strikes(final int count) {
+    private static Consumer<Game> repeat(final int count, final Consumer<Game> action) {
         return game ->
         {
             for (int i = 0; i < count; i++) {
-                game.roll(10);
+                action.accept(game);
             }
         };
     }
 
     private static Consumer<Game> strike() {
-        return game -> game.roll(10);
+        return roll(10);
     }
 
-    private static Consumer<Game> spare(final int pins) {
-        return game -> {
-            game.roll(pins);
-            game.roll(10 - pins);
-        };
+    private static Consumer<Game> spare(final int first) {
+        return roll(first).andThen(roll(10 - first));
     }
 
     private static Consumer<Game> roll(final int pins) {
         return game -> game.roll(pins);
     }
 
-    private static Consumer<Game> rolls(final int count, final int pins) {
-        return game -> {
-            for (int i = 0; i < count; i++) {
-                game.roll(pins);
-            }
-        };
-    }
-
+    @SafeVarargs
     private static int scoreOf(final Consumer<Game>... actions) {
         final Game game = new Game();
         for (final Consumer<Game> action : actions) {
